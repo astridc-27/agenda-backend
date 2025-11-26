136e9f44213f4306c4f927f6026ec5817120df80
astrid.dev <astridcaceres62@gmail.com>
2025-11-26 19:59:20 -0300

    Commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://agenda-frontend-alpha.vercel.app/',
   credentials: true,
 }));
 app.use(express.json());
